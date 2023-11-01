b8098640ea91497e6f9f8938a56b70610c0c02d8
Theodor Larsson <thela038@student.liu.se>
2023-11-01 11:51:25 +0100

    Cleaned up repository

--- MyDemo/MyDemo/MyDemo/MainWindow.xaml.cs
+++ MyDemo/MyDemo/MyDemo/MainWindow.xaml.cs
@@ -1,20 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using ChatApp.Model;
 using ChatApp.View;
-using ChatApp.ViewModel;
 
 namespace ChatApp
 {
@@ -27,7 +13,6 @@ namespace ChatApp
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new MainWindowViewModel(new NetworkManager());
             MainFrame = this.mainFrame;
             MainFrame.NavigationService.Navigate(new LoginPageView());
         }
