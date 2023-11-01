e1a651857987e1959e970a5b24d2a2b6cc31a479
Filip Johnsson <johnsson.filip@protonmail.com>
2023-11-01 14:20:08 +0100

    Network manager

--- MyDemo/MyDemo/MyDemo/Model/NetworkManager.cs
+++ MyDemo/MyDemo/MyDemo/Model/NetworkManager.cs
@@ -1,31 +1,105 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ChatApp.Model
 {
+
     internal class NetworkManager
     {
+        class NoConnectionException : Exception { }
 
-        public bool startConnection()
+        class AlreadyConnectedException : Exception { }
+
+        private TcpClient? tcpClient;
+
+        public bool IsConnected => tcpClient != null && tcpClient.Connected;
+
+        public void StartListening(string ip, int port)
         {
-            System.Diagnostics.Debug.WriteLine("Starting a connection...");
-            System.Diagnostics.Debug.WriteLine("Connection established!");
-            return true;
+            if (IsConnected)
+            {
+                throw new AlreadyConnectedException();
+            }
 
+            Console.WriteLine("Listening...");
+
+            // Start listening
+            IPAddress ipAddress = IPAddress.Parse(ip);
+            TcpListener listener = new (ipAddress, port);
+            listener.Start();
+
+            // Wait for connection
+            tcpClient = listener.AcceptTcpClient();
+            Console.WriteLine("Connected!");
+        }
+
+        public bool Connect(string ip, int port)
+        {
+            if (IsConnected)
+            {
+                throw new AlreadyConnectedException();
+            }
+
+            // Connect to server
+            try
+            {
+                tcpClient = new TcpClient(ip, port);
+                return true;
+            }
+            catch (SocketException)
+            {
+                return false;
+            }
+        }
+
+        public string ReadMessage()
+        {
+            if (!IsConnected)
+            {
+                throw new NoConnectionException();
+            }
+
+            // Get a stream object for reading and writing
+            NetworkStream networkStream = tcpClient.GetStream();
+
+            // Read data to buffer
+            byte[] buffer = new byte[tcpClient.ReceiveBufferSize];
+            int bytesRead = networkStream.Read(buffer, 0, tcpClient.ReceiveBufferSize);
+
+            // Convert to string
+            string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            Console.WriteLine("Received : " + dataReceived);
+
+            return dataReceived;
         }
 
 
-        public void sendMessage(string str)
+        public void SendMessage(string str)
         {
-            System.Diagnostics.Debug.WriteLine(str + " is sent!");
-            
+            if (!IsConnected)
+            {
+                throw new NoConnectionException();
+            }
+
+            // Get a stream object for reading and writing
+            NetworkStream networkStream = tcpClient.GetStream();
+
+            // Convert to bytes
+            byte[] buffer = Encoding.UTF8.GetBytes(str);
+
+            // Send data
+            networkStream.Write(buffer, 0, buffer.Length);
+
+            Console.WriteLine("Sent : " + str); 
+        }
+
+        public void CloseConnection()
+        {
+            tcpClient?.Close();
+            tcpClient = null;
         }
     }
 }
